a50a74c218f814b696e1bbad8f3257cfee6b6680
Vernon Wong <vrnnwng@gmail.com>
2015-01-09 21:48:10 -0800

    Textual Changes
    Updated About Text and Name variable

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -8,5 +8,4 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<p>Hopscotch is an app that utilizes your Google Calendar profile to assist users in planning events. Users can filter through interests, friends, and dates to collect possible events to attend. This app will be a social diving board for users to stop finding time to plan for events and actually cement the plans.</p>
