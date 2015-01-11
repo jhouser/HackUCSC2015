6224861df3a918f94384b374013c1d4b10437c6b
Jake Houser <apollo4221@gmail.com>
2015-01-10 23:16:45 -0800

    Updated event CSS

--- protected/views/site/_event.php
+++ protected/views/site/_event.php
@@ -1,49 +1,44 @@
-<?php ?>
+<div class="event">
+    <?php
+    $format = "D g:i A";
+    $start = date($format, $data->startTime);
+    $end = date($format, $data->endTime);
 
-<link rel="stylesheet" type="text/css" href="css/event.css">
-
-
-
-<?php
-$format = "D g:i A";
-$start = date($format, $data->startTime);
-$end = date($format, $data->endTime);
-
-$user = Yii::app()->user->getModel();
-$counter = 0;
-$imgString = "";
-foreach ($user->userFriends as $userFriend) {
-    $friend = $userFriend->friend;
-    if ($friend->isAvailable($data)) {
-        $counter++;
-        if ($counter <= 4) {
-            $imgString .= CHtml::image($friend->picture, '', array('height' => '30px', 'width' => '30px','class'=>'mini-prof-pic'));
-        }elseif($counter == 4){
-            $imgString.="+";
+    $user = Yii::app()->user->getModel();
+    $counter = 0;
+    $imgString = "";
+    foreach ($user->userFriends as $userFriend) {
+        $friend = $userFriend->friend;
+        if ($friend->isAvailable($data)) {
+            $counter++;
+            if ($counter <= 4) {
+                $imgString .= CHtml::image($friend->picture, '', array('height' => '30px', 'width' => '30px', 'class' => 'mini-prof-pic'));
+            } elseif ($counter == 4) {
+                $imgString.="+";
+            }
         }
     }
-}
-?>
-<div class="icon">
-  <?php 
-  $imgPath = "images/".$data->type->type.".gif";
-  echo CHtml::image($imgPath);
-  ?>
-</div>
-<div class="text">
-    <h2><?php echo $data->title; ?></h1>
-        <p><?php echo $data->description; ?></p>
-        <p><?php echo $start . " - " . $end; ?></p>
-</div>
-<div class="friend">
-    <div>
-        <?php echo $counter . " : Friends can attend" ?>
+    ?>
+    <div class="icon">
+        <?php
+        $imgPath = "images/" . $data->type->type . ".gif";
+        echo CHtml::image($imgPath);
+        ?>
     </div>
-    <div>
-        <?php echo $imgString; ?>
+    <div class="text">
+        <h2><?php echo $data->title; ?></h1>
+            <p><?php echo $data->description; ?></p>
+            <p><?php echo $start . " - " . $end; ?></p>
     </div>
-</div>
+    <div class="friend">
+        <div>
+            <?php echo $counter . " : Friends can attend" ?>
+        </div>
+        <div>
+            <?php echo $imgString; ?>
+        </div>
+    </div>
 
-<br>
+</div>
 
 
--- protected/views/site/results.php
+++ protected/views/site/results.php
@@ -5,6 +5,7 @@
  * Will display all the events available to the user.
  */
 ?>
+<link rel="stylesheet" type="text/css" href="css/event.css">
 <div class="page-title"><h1>Available Events</h1></div>
 <?php
 
