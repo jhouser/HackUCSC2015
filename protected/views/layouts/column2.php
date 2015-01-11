cf965b5fdd96494e19c3ceef1aa7acc5d341ed17
Jake Houser <apollo4221@gmail.com>
2015-01-10 22:39:42 -0800

    Added sidebar.

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -1,22 +1,21 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="span-19">
-	<div id="content">
-		<?php echo $content; ?>
-	</div><!-- content -->
+<div class="span-5">
+    <div id="sidebar">
+        <div id ="profile">
+            <div id="profile-picture">
+                <?php echo CHtml::image(Yii::app()->user->picture); ?>
+            </div>
+            <span id="profile-name">
+                <?php echo Yii::app()->user->fullName; ?>
+            </span>
+        </div>
+    </div><!-- sidebar -->
 </div>
-<div class="span-5 last">
-	<div id="sidebar">
-	<?php
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->widget('zii.widgets.CMenu', array(
-			'items'=>$this->menu,
-			'htmlOptions'=>array('class'=>'operations'),
-		));
-		$this->endWidget();
-	?>
-	</div><!-- sidebar -->
+<div class="span-19 last">
+    <div id="content">
+        <?php echo $content; ?>
+    </div><!-- content -->
 </div>
+
 <?php $this->endContent(); ?>
